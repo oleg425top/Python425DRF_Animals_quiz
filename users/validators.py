32298102cb7c808de37ee1a9bb1b2812893391da
oleg425top <oleg59152@gmail.com>
2025-05-18 15:41:46 +0500

    Добавлен валидатор пароля пользователя Добавлен сериализаторы для пользователя

---
+++ users/validators.py
@@ -0,0 +1,13 @@
+import re
+from django.core.exceptions import ValidationError
+
+class PasswordValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        reg_pattern = re.compile(r'^[A-Za-z0-9]+$')
+        temp_value = dict(value).get(self.field)
+        if not bool(reg_pattern.match(temp_value)):
+            raise ValidationError('Ошибка: Пароль должен содержать только латинские буквы и цифры')
