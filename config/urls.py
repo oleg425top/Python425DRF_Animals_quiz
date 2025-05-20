cce263c108d5ded603103e2ce2c2327b38fe0617
oleg425top <oleg59152@gmail.com>
2025-05-20 14:36:49 +0500

    Написан CRUD для модели Section Прописаны urls для sections Обновлены базовые urls

--- config/urls.py
+++ config/urls.py
@@ -35,20 +35,20 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny,],
+    permission_classes=[permissions.AllowAny, ],
 )
 
 urlpatterns = [
-    #base
-    path('admin/', admin.site.urls),
+                  # base
+                  path('admin/', admin.site.urls),
 
-    #documentation urls
+                  # apps urls
+                  path('', include('sections.urls', namespace='sections')),
+                  path('users/', include('users.urls', namespace='users')),
 
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),
+                  # documentation urls
+                  path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    #apps urls
-    path('users/', include('users.urls', namespace='users')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ sections/urls.py
@@ -0,0 +1,29 @@
+from os import path as p
+from tkinter.font import names
+
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from sections.apps import SectionsConfig
+from sections.views import SectionListAPIView, SectionCreateAPIView, SectionUpdateAPIView, SectionRetrieveAPIView, \
+    SectionDestroyAPIView
+
+app_name = SectionsConfig.name
+
+router = DefaultRouter()
+
+section = 'section/'
+content = 'content/'
+question = 'question/'
+create = 'create/'
+update = 'update/'
+delete = 'delete/'
+int_pk = '<int:pk>'
+
+urlpatterns = [
+                  path(p.join(section), SectionListAPIView.as_view(), name='section_list'),
+                  path(p.join(section, create), SectionCreateAPIView.as_view(), name='section_create'),
+                  path(p.join(section, int_pk), SectionRetrieveAPIView.as_view(), name='section_detail'),
+                  path(p.join(section, update), SectionUpdateAPIView.as_view(), name='section_update'),
+                  path(p.join(section, delete), SectionDestroyAPIView.as_view(), name='section_delete'),
+              ] + router.urls
